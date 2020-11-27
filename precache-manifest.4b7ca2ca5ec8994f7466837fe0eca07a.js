e54bca9a73c83dee3287817d6c997035c8bc8629
Andrii Krychkivskyi <kruchandriy@gmail.com>
2020-11-27 10:03:29 +0200

    Updates

---
+++ precache-manifest.4b7ca2ca5ec8994f7466837fe0eca07a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f960e740fa1a229d57a86d85bcef7eae",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "7de13a832c222048e895",
+    "url": "/goit-react-hw-01-components/static/js/2.ea61eacd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/goit-react-hw-01-components/static/js/2.ea61eacd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4406d133c1bd2dfc5e86",
+    "url": "/goit-react-hw-01-components/static/js/main.56cbed95.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
